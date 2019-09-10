1b0172682070f67dcf034a86f6e5a8f7fe6f4425
Anurag Bhandari <anurag.bhd@gmail.com>
2019-09-10 22:54:50 +0530

    Add spinner on loading screen
    Once weather data is downloaded, redirect to LocationScreen.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:clima/utilities/constants.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/screens/location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -23,6 +25,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
             'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kOpenWeatherApiKey');
 
     var weatherData = await netHelper.getData();
+
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LocationScreen()));
   }
 
   @override
@@ -33,6 +38,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitCubeGrid(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
